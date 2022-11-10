a96e2b18ce07b9f1da6f082ea1833a4a8f7bb241
rustam-kenzhali <74444247+rustam-kenzhali@users.noreply.github.com>
2022-11-10 10:07:25 +0600

    Update main.py

--- flask_web/main.py
+++ flask_web/main.py
@@ -4,5 +4,5 @@ from .website import create_app
 app = create_app()
 
 if __name__ == '__main__':  # only if we run main.py, not if we import this file
-    app.run(debug=True)     # we execute this line
+    app.run(debug=False)     # we execute this line
     # debug = True : mean that every change in the code automaticli rerun the webserver
